120c80c19e7131072f06094315b85f481f0fd3f2
Obraztsov Oleg Aleksandrovich <olegrasputina@gmail.com>
2025-08-03 15:18:02 +0300

    [*]

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use async_trait::async_trait;
+use serde_json::Value;
+use std::error::Error;
+
+#[async_trait]
+pub trait Tool: Send + Sync {
+    fn name(&self) -> &str;
+    fn description(&self) -> &str;
+    fn parameters(&self) -> Value;
+    async fn execute(&self, arguments: Value) -> Result<Value, Box<dyn Error>>;
+}
+
+
+pub type CreateToolFn = unsafe extern fn() -> *mut dyn Tool;
