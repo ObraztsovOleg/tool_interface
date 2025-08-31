3f69b6673a96d7c80531f42e8397b21cc1a44778
Obraztsov Oleg Aleksandrovich <olegrasputina@gmail.com>
2025-08-31 18:09:01 +0300

    [*]

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,18 @@ use async_trait::async_trait;
 use serde_json::Value;
 use std::error::Error;
 
+pub struct ToolError {
+    pub error: String,
+    pub suggestion: String,
+    pub status: String,
+}
+
 #[async_trait]
 pub trait Tool: Send + Sync {
     fn name(&self) -> &str;
     fn description(&self) -> &str;
     fn parameters(&self) -> Value;
-    async fn execute(&self, arguments: Value) -> Result<Value, Box<dyn Error>>;
+    async fn execute(&self, arguments: Value) -> Result<Value, ToolError>;
 }
 
 
